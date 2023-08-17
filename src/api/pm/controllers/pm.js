84a86833b28680b9b32c533a9a506c1deb3367cd
somanwai08 <somanwai@hotmail.com.hk>
2023-08-17 14:52:27 +0800

    deploy

---
+++ src/api/pm/controllers/pm.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * pm controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::pm.pm');
---
+++ src/api/pm/services/pm.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * pm service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::pm.pm');
