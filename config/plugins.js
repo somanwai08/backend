d83c4736836f07f75281bb198cd21563125709c2
somanwai08 <somanwai@hotmail.com.hk>
2023-09-14 13:07:37 +0800

    custom rounte and controller

--- config/plugins.js
+++ config/plugins.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     'users-permissions': {
       config: {
         jwt: {
-          expiresIn: '3h',
+          expiresIn: 60,
         },
       },
     },
---
+++ src/extensions/custom-auth/controllers/CustomAuth.js
@@ -0,0 +1,29 @@
+const jwt=require('jsonwebtoken')
+
+module.exports={
+    async login2(ctx){
+
+          // 从请求体中获取用户名和密码
+    const { identifier, password } = ctx.request.body;
+    console.log(ctx,'ctx');
+
+    try {
+      // 在这里编写验证逻辑，验证用户名和密码是否正确
+      // 你可以使用 Strapi 提供的身份验证方法或自定义的逻辑进行验证
+
+      // 假设验证成功
+      if (identifier === 'admin' && password === 'sauvereign123') {
+        // 生成适用于第二个登录页面的 JWT
+        const token = jwt.sign({ identifier }, process.env.JWT_SECRET, { expiresIn: '30' });
+
+        // 返回 JWT
+        ctx.send({ token });
+      } else {
+        // 验证失败
+        ctx.throw(401, 'Invalid credentials');
+      }
+    } catch (error) {
+      ctx.throw(500, 'Internal server error');
+    }
+  }
+}
